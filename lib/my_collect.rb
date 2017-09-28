bf9035754a411e94880a9949813dac3d1f8ae9d6
Winter LaMon <winter.lamon@gmail.com>
2017-09-28 21:41:49 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
-
+def my_collect(collection)
+  new_collection = []
+  i = 0
+  while i < collection.length
+    yield collection[i]
+    new_collection.push(yield collection[i])
+    i += 1
+  end
+  new_collection
+end
