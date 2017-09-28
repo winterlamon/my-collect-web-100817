3aa99855be25804cf41279ebcf8bb8c99c069122
Winter LaMon <winter.lamon@gmail.com>
2017-09-28 21:39:37 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(collection)
   new_collection = []
   i = 0
   while i < collection.length
-    yield collection[i])
+    yield collection[i]
     new_collection << yield collection[i]
     i += 1
   end
